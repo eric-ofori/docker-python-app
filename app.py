c8567f6f66444ba67e7ef6d2334cde5e01849151
eric-ofori <100205237+eric-ofori@users.noreply.github.com>
2025-06-23 21:51:03 +0000

    docker-build

--- app.py
+++ app.py
@@ -2,9 +2,11 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+
 @app.get("/")
 def read_root():
     return {"message": "Hello, Dockerized FastAPI!"}
+
 
 @app.get("/health")
 def health_check():
